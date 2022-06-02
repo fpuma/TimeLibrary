a485fddaa30231604d0a65a97ebafdedd4b5be94
fpuma <fpuma88@gmail.com>
2022-06-02 21:07:58 +0200

    Changed the way we calculate the average delta time

--- source/time/public/time/deltatime.h
+++ source/time/public/time/deltatime.h
@@ -9,9 +9,6 @@
 
 namespace puma
 {
-
-    constexpr unsigned int kAverageFpsSampleCount = 30;
-
     class DeltaTime
     {
 
@@ -20,33 +17,31 @@ namespace puma
         DeltaTime()
             : m_timeReference()
             , m_previousExecutionSeconds( m_timeReference.getElapsedSeconds() )
-            , m_dtSamples()
         {}
 
         virtual ~DeltaTime() = default;
      
-        float get() const { return m_dtSamples[m_currentDtIndex]; }
+        float get() const { return static_cast<float>(m_currentDt); }
 
-        float getAverage() const
-        {
-            float dtSum = std::accumulate( m_dtSamples.begin(), m_dtSamples.end(), 0.0f );
-            return dtSum / (float)m_dtSamples.size();
-        }
+        float getAverage() const { return static_cast<float>(m_averageDt); }
 
         void update()
         {
-            m_currentDtIndex = ++m_currentDtIndex < m_dtSamples.size() ? m_currentDtIndex : 0 ;
             float elapsedSeconds = (float)m_timeReference.getElapsedSeconds();
-            m_dtSamples[m_currentDtIndex] = (float)(elapsedSeconds - m_previousExecutionSeconds);
+            m_currentDt = (float)(elapsedSeconds - m_previousExecutionSeconds);
             m_previousExecutionSeconds = elapsedSeconds;
+
+            ++m_updateCount;
+            m_averageDt += (m_currentDt - m_averageDt) / m_updateCount;
         }
 
     private:
 
         TimeReference m_timeReference;
+        double m_currentDt = 0.0f;
         double m_previousExecutionSeconds = 0.0;
-        std::array<float, kAverageFpsSampleCount> m_dtSamples;
-        unsigned int m_currentDtIndex = 0;
+        double m_averageDt = 0.0f;
+        unsigned int m_updateCount = 0;
     };
 
 }
